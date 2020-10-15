5a2f4ca8a06acaa5cf492d2eff3730118d5b9ca3
Jasper Sands <jaspersands@Jaspers-MacBook-Pro.local>
2020-10-15 13:45:00 -0700

    sup

--- puzzle.py
+++ puzzle.py
@@ -1,2 +1,12 @@
 #Japer Dan Colin maybe Akis n Kunde
-def puzzleshit():
+def LoadFromFile(filepath):
+	state = []
+	f = open(filepath, "r")
+	for i in f:
+		state.append(i.strip().split("\t"))
+	dimensions = state.pop(0)[0]
+	print(state)
+	print(dimensions)
+
+
+LoadFromFile("Untitled.txt")
